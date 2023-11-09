b9bb36f02c9d97721be8ebc0a2fe9f06844b71b7
prithul-msspl <prithul.paul@massoftind.com.com>
2023-11-09 16:28:29 +0530

    Git Push On 9th Nov, 2023

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,25 @@
-// Put your application javascript here
+jQuery(document).ready(function($){
+    $(document).on("submit", "#cart_from", function(event){
+        var notification_selector = $(this);
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: '/cart/add.js',
+            cache: false,
+            data: $(this).serialize(),
+            dataType: "json",
+            success: function(data)
+            {
+                var cart_count = $("li#cart_item_count").attr("data-cart");
+
+                $("li#cart_item_count a").html("Cart("+(parseInt(cart_count)+1)+")");
+                $("li#cart_item_count").attr('data-cart', (parseInt(cart_count)+1));
+                $(notification_selector).next("div.product_notification").html("Added To Cart !");
+                setTimeout(function(){
+                    $(notification_selector).next("div.product_notification").html("");
+                }, 3000);
+                // console.log(data.item_count);
+            }
+        });
+    });
+});
